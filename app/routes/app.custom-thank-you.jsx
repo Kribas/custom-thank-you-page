a22afcbfa1f6f91d8ec257a5dcbe1bad88ef7b65
Kribas <carlousrimal@gmail.com>
2025-10-28 09:25:26 +0545

    added textfield form validation

--- app/routes/app.custom-thank-you.jsx
+++ app/routes/app.custom-thank-you.jsx
@@ -16,13 +16,13 @@ import {
 } from "@shopify/polaris";
 import { TitleBar, useAppBridge } from "@shopify/app-bridge-react";
 import { authenticate } from "../shopify.server";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useActionData, Form } from "@remix-run/react";
 
 import prisma from "../db.server";
 
 export async function action({ request }) {
-  const { admin, session } = await authenticate.admin(request);
+  const { session } = await authenticate.admin(request);
   const shop = session.shop;
   const formData = await request.formData();
   const message = formData.get("message");
@@ -42,6 +42,8 @@ export default function AdditionalPage() {
   const [message, setMessage] = useState("");
   const [discountCode, setDiscountCode] = useState("");
   const [activeToast, setActiveToast] = useState(false);
+  const [messageError, setMessageError] = useState("");
+  const [discountCodeError, setDiscountCodeError] = useState("");
 
   const toggleActiveToast = useCallback(
     () => setActiveToast((activeToast) => !activeToast),
@@ -59,42 +61,74 @@ export default function AdditionalPage() {
       <Toast content="Settings saved!" onDismiss={toggleActiveToast} />
     ) : null;
 
+  useEffect(() => {
+    if (actionData?.success) {
+      setMessage("");
+      setDiscountCode("");
+    }
+  }, [actionData]);
+
+  const handleFormSubmit = (event) => {
+    let hasError = false;
+    if (!message.trim()) {
+      setMessageError("This field cannot be empty!");
+      hasError = true;
+    } else {
+      setMessageError("");
+    }
+
+    if (!discountCode.trim()) {
+      setDiscountCodeError("This field cannot be empty!");
+      hasError(true);
+    } else {
+      setDiscountCodeError("");
+    }
+
+    if (hasError) {
+      event.preventDefault();
+    }
+  };
+
   return (
-    <Page title="Custom Thank you page">
-      <ui-title-bar title="Custom Thank you page"></ui-title-bar>
-      <Card>
-        <Form method="POST">
-          <BlockStack gap="500">
-            <Text as="h2">
-              Add a personalized message, image, or discount for your customers
-              after checkout.
-            </Text>
-            <TextField
-              onChange={handleMessageChange}
-              name="message"
-              label="Message to display"
-              value={message}
-            />
-            <TextField
-              onChange={handleDiscountCodeChange}
-              name="discountCode"
-              label="Discount Code"
-              value={discountCode}
-            />
-            <BlockStack gap="100">
-              <Text>Thank you for your order!</Text>
-              <Text>
-                We appreciate you buying from us. Use code WELCOME10 for 10% off
-                your next purchase.
+    <Frame>
+      <Page title="Custom Thank you page">
+        <ui-title-bar title="Custom Thank you page"></ui-title-bar>
+        <Card>
+          <Form method="POST" onSubmit={handleFormSubmit}>
+            <BlockStack gap="500">
+              <Text as="h2">
+                Add a personalized message, image, or discount for your
+                customers after checkout.
               </Text>
+              <TextField
+                onChange={handleMessageChange}
+                name="message"
+                label="Message to display"
+                value={message}
+                error={messageError}
+              />
+              <TextField
+                onChange={handleDiscountCodeChange}
+                name="discountCode"
+                label="Discount Code"
+                value={discountCode}
+                error={discountCodeError}
+              />
+              <BlockStack gap="100">
+                <Text>Thank you for your order!</Text>
+                <Text>
+                  We appreciate you buying from us. Use code WELCOME10 for 10%
+                  off your next purchase.
+                </Text>
+              </BlockStack>
+              <Button onClick={toggleActiveToast} submit variant="primary">
+                Save
+              </Button>
             </BlockStack>
-            <Button onClick={toggleActiveToast} submit variant="primary">
-              Save
-            </Button>
-          </BlockStack>
-        </Form>
-      </Card>
-      <Frame>{toastMarkup}</Frame>
-    </Page>
+          </Form>
+        </Card>
+        {toastMarkup}
+      </Page>
+    </Frame>
   );
 }
